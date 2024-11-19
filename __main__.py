41dfa14d755839ac9d597f510f5d0778a9d2649e
Mike Pagel <mike@mpagel.de>
2024-11-19 18:16:47 +0100

    create VM from cloud image with test user account

--- __main__.py
+++ __main__.py
@@ -13,6 +13,10 @@ provider = pulumi_proxmoxve.Provider(
     endpoint=proxmox_stack_prod.get_output('api-endpoint'),
     api_token=proxmox_stack_prod.get_output('api-token'),
     insecure=proxmox_stack_prod.get_output('api-insecure'),
+    ssh=pulumi_proxmoxve.ProviderSshArgs(
+        username=proxmox_stack_prod.get_output('ssh-user'),
+        private_key=proxmox_stack_prod.get_output('ssh-private-key'),
+    ),
 )
 
 pve_config = config.require_secret_object('pve')
@@ -29,21 +33,49 @@ cloud_image = pulumi_proxmoxve.download.File(
     opts=pulumi.ResourceOptions(provider=provider),
 )
 
+master_config = config.require_object('master-0')
+master_name = f'k8s-master-{stack_name}-0'
 
-# master_vm = pulumi_proxmoxve.vm.VirtualMachine(
-#     f'k8s-master-0-{stack_name}',
-#     node_name=node_name,
-#     description='Kubernetes Master, maintained with Pulumi.',
-#     cpu=pulumi_proxmoxve.vm.VirtualMachineCpuArgs(cores=2),
-#     memory=pulumi_proxmoxve.vm.VirtualMachineMemoryArgs(dedicated=2048),
-#     disks=[
-#         pulumi_proxmoxve.vm.VirtualMachineDiskArgs(interface='scsi0', size=8, file_format='raw'),
-#     ],
-#     cdrom=pulumi_proxmoxve.vm.VirtualMachineCdromArgs(
-#         enabled=True, file_id='local:iso/ubuntu-24.04.1-live-server-amd64.iso'
-#     ),
-#     network_devices=[pulumi_proxmoxve.vm.VirtualMachineNetworkDeviceArgs(bridge='vmbr0')],
-#     opts=pulumi.ResourceOptions(provider=provider),
-# )
+master_vm = pulumi_proxmoxve.vm.VirtualMachine(
+    master_name,
+    name=master_name,
+    vm_id=master_config['vmid'],
+    tags=[stack_name],
+    node_name=node_name,
+    description='Kubernetes Master, maintained with Pulumi.',
+    # cpu=pulumi_proxmoxve.vm.VirtualMachineCpuArgs(cores=2),
+    # memory=pulumi_proxmoxve.vm.VirtualMachineMemoryArgs(dedicated=2048),
+    cdrom=pulumi_proxmoxve.vm.VirtualMachineCdromArgs(enabled=False),
+    disks=[
+        pulumi_proxmoxve.vm.VirtualMachineDiskArgs(
+            interface='virtio0',
+            size=8,
+            file_id=cloud_image.id,
+            iothread=True,
+            discard='on',
+        ),
+    ],
+    network_devices=[pulumi_proxmoxve.vm.VirtualMachineNetworkDeviceArgs(bridge='vmbr0')],
+    # cannot be activated before qemu agent is installed agent=pulumi_proxmoxve.vm.VirtualMachineAgentArgs(enabled=True),
+    initialization=pulumi_proxmoxve.vm.VirtualMachineInitializationArgs(
+        ip_configs=[
+            pulumi_proxmoxve.vm.VirtualMachineInitializationIpConfigArgs(
+                ipv4=pulumi_proxmoxve.vm.VirtualMachineInitializationIpConfigIpv4Args(
+                    address='dhcp'
+                )
+            )
+        ],
+        user_account=pulumi_proxmoxve.vm.VirtualMachineInitializationUserAccountArgs(
+            username='root',
+            password='holladiewaldfee',
+        ),
+    ),
+    opts=pulumi.ResourceOptions(
+        provider=provider,
+        # disks and cdrom has contant diffs and lead to update errors, possibly a bug in provider:
+        ignore_changes=['disks', 'cdrom'],
+    ),
+)
+
 
 # pulumi.export('vm IPs', master_vm.ipv4_addresses)
